a7c162c5bf62f84b41dab415aab542a8a700689d
Sevvlor <sevvlor@users.noreply.github.com>
2016-05-17 22:20:27 +0200

    Imprved code
    I have added some comments on better ways to do things, this does not mean the code is production ready, **only** for demo ;)

--- signup.php
+++ signup.php
@@ -1,59 +1,62 @@
 <?php
 
+/*
+ * PDO Database connection, can be placed in separate file
+ * Documentation: http://php.net/manual/en/pdo.connections.php
+ * */
 
-// script by zerox
+$db = new PDO('mysql:host=HOST;dbname=DATABASENAME', 'USERNAME', 'PASSWORD');
+/*
+ * Verify if a username is already taken,
+ * Used PDO instead of the deprecated mysql_* function used in previous code
+ * Documentation PDO::PREPARE http://php.net/manual/en/pdo.prepare.php
+ * documentation PDO::execute
+ *
+ * */
+$getUser = $db->prepare("SELECT * FROM clients WHERE username = :username ");
+$getUser->execute(array(":username" => $_POST["username"]));//This will return false if query failed
+/*
+ * Check if rowcount is above 0, if so the username is taken
+ * */
+if($getUser->rowCount() == 1) {
+    echo "That username has been taken.";
+} else if(!preg_match('/^[a-zA-Z0-9]+$/', $_POST["username"])) {// If our username is invalid
+    echo "The username can only contain letters or numbers."; // Tell the user
 
-//mysql_real_escape_string = veiligheid laten staan.
-// database file.
-
-require('data.php');
-
-$username = $_POST["username"];
-$password =$_POST["password"];
-//md5 is voor encryptie
-$pw		  = md5 ($password);
-$email 	  = $_POST(["email"];
-$bank	  =	$_POST(["bank"];
+    /*
+     * Previous code used deprecated function, switched to PHP's filter_var()
+     * Documentation: http://php.net/manual/en/function.filter-var.php
+     * */
+} else if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false) {
+    echo "The email or PayPal address you entered is invalid."; // Tell the user
+} else {
 
 
-// controlle op gebruikte naam.
+    /*
+     * Previous code used MD5, which is flawed. Read this why:
+     * - https://www.sitepoint.com/why-you-should-use-bcrypt-to-hash-stored-passwords/
+     * - http://stackoverflow.com/a/30949746/1604068
+     * - http://security.stackexchange.com/a/5691/105284
+     *
+     * Documentation php.net/manual/en/function.password-hash.php for password_hash() and password_verify()
+     */
 
-	$query = "SELECT * FROM clients";
-	$result= mysql_query($query) or die (mysql_error()); // lijst met clienten.
-	while($row = mysql_fetch_array($result)){ //voor iedere invoer controleer gebruikernaam.
-			if ($row['username'] == $username){
-				$usernameTaken = true;
-			}else{$usernameTaken = false;}
-	}
-//controlle
-if($usernameTaken)
-{
-	echo "That username has been taken.";
-}
+    $createUser = $db->prepare("INSERT INTO clients (:username, :password, :email, :bank)");
+    $createCheck = $createUser->execute(array(
+        ":username"     => $_POST["username"],
+        ":password"     => password_hash($_POST['password'], PASSWORD_DEFAULT),
+        ":email"        => $_POST['email'],
+        ":bank"         => $_POST['bank']
+    ));
 
-else if(!preg_match('/^[a-zA-Z0-9]+$/', $username)) // If our username is invalid
-{
-     echo "The username can only contain letters or numbers."; // Tell the user
-}
-
-else if(!preg_match('/^[a-zA-Z0-9]+$/', $password)) // If our password is invalid
-{
-     echo "The password can only contain letters or numbers."; // Tell the user
-}
-// If our email or PayPal addresses are invalid
-else if(!eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $email))
-{
-     echo "The email or PayPal address you entered is invalid."; // Tell the user
-}
-else{
-
-// Inserts the data into the database
-	/** @noinspection PhpDeprecationInspection */
-	$result= MYSQL_QUERY(
-		 "INSERT INTO clients (client_ID, username, password, email, bank)".
-		 "VALUES ('', '$username', '$pw', '$email', '$bank')"
-		 );
-echo "Thank you for signing up.";
+    /*
+     * Check if user actually has been created
+     * */
+    if($createCheck == false)
+    {
+        throw new Exception('Failed to create user '.$createUser->errorInfo());
+    }
+    echo "Thank you for signing up.";
 
 }
 
